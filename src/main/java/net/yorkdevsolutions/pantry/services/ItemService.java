882a8d16aeb19c54a91cc49f5bc09e309d0565a2
Luke Pilon <luke.pilon@gmail.com>
2023-01-14 13:54:40 -0600

    item service for crud operations and adding new items from a recipe

---
+++ src/main/java/net/yorkdevsolutions/pantry/services/ItemService.java
@@ -0,0 +1,86 @@
+package net.yorkdevsolutions.pantry.services;
+
+import net.yorkdevsolutions.pantry.entities.Item;
+import net.yorkdevsolutions.pantry.entities.Recipe;
+import net.yorkdevsolutions.pantry.entities.RecipeIngredient;
+import net.yorkdevsolutions.pantry.repositories.ItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+@Service
+public class ItemService {
+    private final ItemRepository itemRepository;
+    private final RecipeService recipeService;
+
+    public ItemService(ItemRepository itemRepository, RecipeService recipeService) {
+        this.itemRepository = itemRepository;
+        this.recipeService = recipeService;
+    }
+
+    public Iterable<Item> getAllItems(){
+        return this.itemRepository.findAll();
+    }
+
+    public Item createItemFromNewRecipe(String itemName) {
+        var newItem = new Item(itemName);
+        return this.itemRepository.save(newItem);
+    }
+
+    public Item getItemById(Long itemId){
+        return this.itemRepository.findById(itemId).orElse(null);
+    }
+
+    public Item createItem(Item item) {
+        if(!this.itemRepository.existsByName(item.getName())){
+            return this.itemRepository.save(item);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Item updateItem(Long itemId, Item item){
+        Item existingItem = this.itemRepository.findById(itemId).orElse(null);
+        if(existingItem == null){
+            return this.createItem(item);
+        }
+        if(!existingItem.getId().equals(item.getId())){
+            throw new IllegalArgumentException();
+        }
+        return this.itemRepository.save(item);
+    }
+
+    public void deleteItemById(Long itemId) {
+        itemRepository.delete(itemRepository.findById(itemId).orElseThrow());
+    }
+
+    public void updatePantryQuantitiesFromRecipe(Long recipeId) {
+        Recipe recipe = this.recipeService.findRecipeById(recipeId);
+        //TODO
+        //Make this a transaction instead
+        for(RecipeIngredient ingredient : recipe.getIngredients()){
+            if(ingredient.getQuantity() > ingredient.getItem().getUnitsAvailable()){
+                throw new IllegalArgumentException();
+            }
+        }
+        for(RecipeIngredient ingredient : recipe.getIngredients()) {
+            Long newUnitsAvailable = ingredient.getItem().getUnitsAvailable() - ingredient.getQuantity();
+            ingredient.getItem().setUnitsAvailable(newUnitsAvailable);
+        }
+    }
+
+    public Iterable<Item> updateMultipleItemQuantities(Map<Item, Long> itemsToAdd) {
+        var itemsUpdated = new ArrayList<Item>();
+        for(Map.Entry<Item,Long> entry : itemsToAdd.entrySet()){
+            Item item = entry.getKey();
+            if(entry.getValue() > 0){
+                Long newQuantity = item.getUnitsAvailable() + entry.getValue();
+                item.setUnitsAvailable(newQuantity);
+            }
+            itemsUpdated.add(item);
+            itemRepository.save(item);
+        }
+        return itemsUpdated;
+    }
+}
