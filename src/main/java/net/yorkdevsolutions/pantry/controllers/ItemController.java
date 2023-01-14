f74490b70ae75495ce052a04151bd4b14e7b4663
Luke Pilon <luke.pilon@gmail.com>
2023-01-14 13:51:44 -0600

    adding controllers for http crud operations

---
+++ src/main/java/net/yorkdevsolutions/pantry/controllers/ItemController.java
@@ -0,0 +1,73 @@
+package net.yorkdevsolutions.pantry.controllers;
+
+import net.yorkdevsolutions.pantry.entities.Item;
+import net.yorkdevsolutions.pantry.services.ItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/items")
+@CrossOrigin
+public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public Iterable<Item> getAllItems(){
+        return this.itemService.getAllItems();
+    }
+
+    @PostMapping
+    public Item createItem(@RequestBody Item item){
+        try {
+            return this.itemService.createItem(item);
+        } catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED);
+        }
+    }
+
+    @PutMapping("/{itemId}")
+    public Item updateItem(@RequestBody Item item, @PathVariable Long itemId){
+        try {
+            return this.itemService.updateItem(itemId, item);
+        } catch (IllegalArgumentException e){
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{itemId}")
+    public void deleteItemById(@PathVariable Long itemId){
+        try {
+            this.itemService.deleteItemById(itemId);
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/recipes/{recipeId}")
+    public void updatePantryQuantitiesFromRecipe(@PathVariable Long recipeId){
+        try {
+            itemService.updatePantryQuantitiesFromRecipe(recipeId);
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/shop")
+    public Iterable<Item> updateMultipleItemQuantities(@RequestBody Map<Item,Long> itemsToAdd){
+        return this.itemService.updateMultipleItemQuantities(itemsToAdd);
+    }
+}
---
+++ src/main/java/net/yorkdevsolutions/pantry/controllers/RecipeController.java
@@ -0,0 +1,66 @@
+package net.yorkdevsolutions.pantry.controllers;
+
+import net.yorkdevsolutions.pantry.dto.RecipeDTO;
+import net.yorkdevsolutions.pantry.entities.Recipe;
+import net.yorkdevsolutions.pantry.services.RecipeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/recipes")
+@CrossOrigin
+public class RecipeController {
+    private final RecipeService service;
+
+    public RecipeController(RecipeService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/accounts/{accountId}")
+    public RecipeDTO createRecipe(@PathVariable Long accountId, @RequestBody RecipeDTO recipeDTO){
+        try {
+            Recipe recipe = this.service.createRecipe(accountId,recipeDTO);
+            return new RecipeDTO(recipe);
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED);
+        }
+    }
+
+    @GetMapping("/{recipeId}")
+    public RecipeDTO getRecipeById(@PathVariable Long recipeId){
+        try {
+            Recipe recipe = this.service.findRecipeById(recipeId);
+            return new RecipeDTO(recipe);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/accounts/{accountId}/recipes/{recipeId}")
+    public RecipeDTO editRecipe(@PathVariable Long recipeId, @PathVariable Long accountId, @RequestBody RecipeDTO updatedRecipe){
+        try {
+            Recipe recipe = service.putRecipe(recipeId, accountId, updatedRecipe);
+            return new RecipeDTO(recipe);
+        } catch(Exception e) {
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED);
+        }
+    }
+
+    @DeleteMapping("/accounts/{accountId}/recipes/{recipeId}")
+    public void deleteRecipe(@PathVariable Long accountId, @PathVariable Long recipeId){
+        try {
+            service.deleteRecipeById(accountId, recipeId);
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
