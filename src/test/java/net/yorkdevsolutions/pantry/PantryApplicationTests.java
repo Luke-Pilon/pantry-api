ec2bfef226197ca908c2782158e0e98a2de73bc9
Luke Pilon <luke.pilon@gmail.com>
2023-01-14 13:46:23 -0600

    initial project setup

---
+++ src/test/java/net/yorkdevsolutions/pantry/PantryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorkdevsolutions.pantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
