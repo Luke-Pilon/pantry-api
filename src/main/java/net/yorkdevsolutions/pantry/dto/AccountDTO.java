aa7009cb4ccd352dc556426d0fdac76afa43f239
Luke Pilon <luke.pilon@gmail.com>
2023-01-14 13:51:06 -0600

    adding accountDto to get all recipes for an account as dtos

---
+++ src/main/java/net/yorkdevsolutions/pantry/dto/AccountDTO.java
@@ -0,0 +1,45 @@
+package net.yorkdevsolutions.pantry.dto;
+
+import net.yorkdevsolutions.pantry.entities.Account;
+import net.yorkdevsolutions.pantry.entities.Recipe;
+
+import java.util.Set;
+
+public class AccountDTO {
+    private Long id;
+    private String name;
+    private Set<RecipeDTO> recipes;
+
+    public AccountDTO(Account account) {
+        this.id = account.getId();
+        this.name = account.getName();
+        for(Recipe recipe : account.getRecipes()){
+            var recipeDTO = new RecipeDTO(recipe);
+            this.recipes.add(recipeDTO);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<RecipeDTO> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<RecipeDTO> recipes) {
+        this.recipes = recipes;
+    }
+}
